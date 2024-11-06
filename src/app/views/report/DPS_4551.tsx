bffa457e0936d1f306f23eab06a89eac8784debc
zakiulhaq <zakiulhaqhasaan@gmail.com>
2024-11-06 11:34:00 +0430

    DPS report 4551 completed

--- src/app/views/report/DPS_4551.tsx
+++ src/app/views/report/DPS_4551.tsx
@@ -61,53 +61,67 @@ function DPS_4551() {
             stripedRows
             showGridlines
           >
-            <Column field={'sadType'} header={'SAD Type'} />
-            <Column field={'transportCustomsName'} header={'Transport Custom Name'} />
-            <Column filter filterField="customsName" field={'customsName'} header={'Custom Name'} />
-            <Column field={'customsCode'} header={'Custom Code'} />
+            <Column field={'ideTypSad'} header={'ID_TypeSad'} />
+            <Column field={'tptCuoNam'} header={'borderCustom'} />
+            <Column filter filterField="ideCuoNam" field={'ideCuoNam'} header={'Custom Name'} />
+            <Column field={'typeofTransport'} header={'Type of transport'} />
             <Column field={'itemTotal'} header={'Item Total'} />
             <Column field={'itemNo'} header={'itemNo'} />
             <Column field={'totalPackage'} header={'Total Package'} />
-            <Column field={'packageCode'} header={'Package Code'} />
-            <Column field={'packageType'} header={'Package Type'} />
-            <Column filter filterField='registrationNo' field={'registrationNo'} header={'RegistrationNo'} />
-            <Column field={'registrationDate'} header={'Registration Date'} />
-            <Column filter filterField='assessmentNumber' field={'assessmentNumber'} header={'Assessment Number'} />
-            <Column field={'assessmentDate'} header={'Assessment Date'} />
-            <Column field={'receiptDate'} header={'Receipt Date'} />
-            <Column field={'bankName'} header={'Bank Name'} />
-            <Column field={'hsCode'} header={'hsCode'} />
-            <Column field={'cpc'} header={'cpc'} />
-            <Column style={{ minWidth: "14rem" }} field={'goodsDescription'} header={'Goods Description'} />
+            <Column field={'codeOfPackage'} header={'Package Code'} />
+            <Column field={'typeOfPackage'} header={'Package Type'} />
+            <Column filter filterField='regNo' field={'regNo'} header={'RegistrationNo'} />
+            <Column field={'regDate'} header={'Registration Date'} />
+            <Column filter filterField='asmtNo' field={'asmtNo'} header={'Assessment Number'} />
+            <Column field={'astDate'} header={'Assessment Date'} />
+            <Column field={'rcptDate'} header={'Receipt Date'} />
+            <Column field={'bankNam'} header={'Bank Name'} />
+            <Column field={'licCod'} header={'Lic_Code'} />
+            <Column field={'txtFre'} header={'Text_Fer'} />
+            {/*  <Column style={{ minWidth: "14rem" }} field={'goodsDescription'} header={'Goods Description'} />
             <Column style={{ minWidth: "20rem" }} field={'goodsAdditionalDesc'} header={'Goods Additional Description'} />
-            <Column style={{ minWidth: "12rem" }} field={'packageMark1'} header={'Package Mark1'} />
-            <Column field={'packageMark2'} header={'Package Mark2'} />
-            <Column field={'customsProcedure'} header={'Customs Procedure'} />
+            <Column style={{ minWidth: "12rem" }} field={'packageMark1'} header={'Package Mark1'} />*/}
+           
+            <Column field={'hsCode'} header={'HS_CODE'} />
+            <Column field={'customsProc'} header={'Customs Procedure'} />
             <Column field={'itemGrossWeight'} header={'Item Gross Weight'} />
             <Column field={'itemNetWeight'} header={'Item Net Weight'} />
-            <Column filter filterField='brokerTIN' field={'brokerTIN'} header={'Broker TIN'} />
-            <Column field={'declarantName'} header={'Declarant Name'} />
-            <Column field={'companyTIN'} header={'Company TIN'} />
-            <Column field={'companyName'} header={'Company Name'} />
-            <Column style={{ minWidth: "12rem" }} field={'financialName'} header={'Financial Name'} />
-            <Column filter filterField='financialCode' field={'financialCode'} header={'Financial Code'} />
-            <Column field={'countryDestinationName'} header={'Country Destination Name'} />
-            <Column field={'countryDestinationCode'} header={'Country Destination Code'} />
-            <Column field={'countryExportName'} header={'Country Export Name'} />
+            <Column filter filterField='brokerTin' field={'brokerTin'} header={'Broker TIN'} />
+            {/* <Column field={'decName'} header={'Declarant Name'} />*/}
+            
+            <Column  filter filterField= 'companyTin' field={'companyTin'} header={'Company TIN'} />
+            <Column field={'cmpNam'} header={'Company Name'} />
+            <Column style={{ minWidth: "12rem" }} field={'finNam'} header={'Financial Name'} />
+            <Column filter filterField='finCod' field={'fisCod'} header={'Financial Code'} />
+            <Column field={'countryDest'} header={'Country Destination Name'} />
+            <Column field={'countryDesCod'} header={'Country Destination Code'} />
+            <Column field={'countryExport'} header={'Country Export Name'} />
             <Column filter filterField='countryExportCode' field={'countryExportCode'} header={'Country Export Code'} />
-            <Column field={'countryOriginName'} header={'Country Origin Name'} />
-            <Column field={'countryOriginCode'} header={'Country Origin Code'} />
+            <Column field={'countryOrg'} header={'Country Origin Name'} />
+           {/* <Column field={'countryOriginCode'} header={'Country Origin Code'} />*/} 
             <Column field={'lorryTotal'} header={'Lorry Total'} />
             <Column field={'currencyCode'} header={'Currency Code'} />
             <Column field={'currencyRate'} header={'Currency Rate'} />
             <Column field={'declarationValueCurrency'} header={'Declaration Value Currency'} />
             <Column field={'declarationValueAfs'} header={'Declaration Value Afs'} />
-            <Column field={'declarationTaxesAfs'} header={'Declaration Taxes Afs'} />
+            <Column field={'declarationTaxes'} header={'Declaration Taxes Afs'} />
             <Column field={'itemValueCurrency'} header={'Item Value Currency'} />
             <Column field={'itemValueAfs'} header={'Item Value Afs'} />
-            <Column field={'itemCIFValue'} header={'Item CIF Value'} />
-            <Column field={'itemTaxesAfs'} header={'Item Taxes Afs'} />
-            <Column field={'taxCodeAmountAfs'} header={'Tax Code Amount Afs'} />
+            <Column field={'itemCifValue'} header={'Item CIF Value'} />
+            <Column field={'status'} header={'status'} />
+            <Column field={'locGoods'} header={'Log Goods'} />
+            <Column field={'licCodItem'} header={'Lic Code Item'} />
+            <Column field={'txtFreItem'} header={'Text Fer Item'} />
+            <Column field={'pckMrk1Second'} header={'package Mark Name'} />
+            <Column field={'pckMrk2Second'} header={'package Mark Code'} />
+            <Column field={'tarVmtFirst'} header={'Tar VmtFirst'} />
+            <Column field={'tarAtt'} header={'TarAtt'} />
+            <Column field={'tarAdt'} header={'TarAdt'} />
+            <Column field={'tarVmtScond'} header={'TarVmt Second'} />
+            <Column field={'gCategoryOfGoods1'} header={'Goods Category 1'} />
+            <Column field={'goodsCategoryOfGoods2'} header={'Goods Category 2'} />
+
+            {/*<Column field={'taxCodeAmountAfs'} header={'Tax Code Amount Afs'} />
             <Column field={'taxCode'} header={'Tax Code'} />
             <Column style={{ minWidth: "12rem" }} field={'taxDescription'} header={'Tax Description'} />
             <Column field={'taxRate'} header={'Tax Rate'} />
@@ -124,11 +138,13 @@ function DPS_4551() {
             <Column filter filterField='box21_2' field={'box21_2'} header={'Box21_2'} />
             <Column field={'sadFlow'} header={'Sad Flow'} />
             <Column field={'locationGoods'} header={'Location Goods'} />
-            <Column field={'goodsCategory1'} header={'Goods Category 1'} />
-            <Column field={'goodsCategory2'} header={'Goods Category 2'} />
+           
             <Column field={'freeText'} header={'Free Text'} />
             <Column field={'ptyDate'} header={'Pty Date'} />
-            <Column field={'decRep'} header={'Declerent Rep'} />
+            <Column field={'decRep'} header={'Declerent Rep'} /> */}
+
+
+            
 
             {/* {reportData.length > 0 &&
                 Object.keys(reportData[0]).map((keys, index) => (
