08c157a75f5fb31d039c4753d7e3bd09de725af2
zakiulhaq <zakiulhaqhasaan@gmail.com>
2024-11-24 10:24:57 +0430

    DPS_4551 cheked

--- src/app/views/report/DPS_4551.tsx
+++ src/app/views/report/DPS_4551.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useRef, useState } from 'react';
-import axios from 'axios';
 import { Box, styled } from '@mui/material';
+import axios from 'axios';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
+import { useEffect, useRef, useState } from 'react';
 import { ReportHeaderInputs, SearchData } from 'src/app/components/report-header-inputs';
 import { SimpleCard } from '../../components';
 import { ROWS_PER_PAGE } from '../../utils/constant';
+//checked
 
 const Container = styled('div')(({ theme }) => ({
     margin: '30px',
@@ -61,16 +62,16 @@ function DPS_4551() {
                         stripedRows
                         showGridlines
                     >
-                        <Column filter filterField="ideTypSad" field={'ideTypSad'} header={'SAD Type'} />
-                        <Column style={{ minWidth: '10rem' }} field={'tptCuoNam'} header={'Border Custom Name'} />
-                        <Column field={'ideCuoNam'} header={'Custom Name'} />
+                        <Column  field={'ideTypSad'} header={'SAD Type'} />
+                        <Column  field={'tptCuoNam'} header={'Border Custom Name'} />
+                        <Column style={{ minWidth: '10rem' }}field={'ideCuoNam'} header={'Custom Name'} />
                         <Column field={'ideCuoCod'} header={'Custom Code'} />
                         <Column field={'typeofTransport'} header={'Type Of Transport'} />
                         <Column field={'itemTotal'} header={'Item Total'} />
                         <Column field={'itemNo'} header={'ItemNo'} />
                         <Column field={'totalPackage'} header={'Total Package'} />
                         <Column field={'codeOfPackage'} header={'Package Code'} />
-                        <Column filter filterField="typeOfPackage" field={'typeOfPackage'} header={'Package Type'} />
+                        <Column  field={'typeOfPackage'} header={'Package Type'} />
                         <Column filter filterField='regNo' field={'regNo'} header={'RegNo'} />
                         <Column field={'regDate'} header={'Registeration Date'} />
                         <Column filter filterField='asmtNo' field={'asmtNo'} header={'AssessNo'} />
@@ -83,22 +84,22 @@ function DPS_4551() {
                         <Column field={'hsCode'} header={'HS_Code'} />
                         <Column field={'hs5'} header={'Hs5'} />
                         <Column field={'cpc'} header={'CPC'} />
-                        <Column style={{ minWidth: '14rem' }} field={'dsc'} header={'Goods Description'} />
-                        <Column style={{ minWidth: '18rem' }} field={'gdsDs3'} header={'Goods Additional Description'} />
+                        <Column style={{ minWidth: '20rem' }} field={'dsc'} header={'Goods Description'} />
+                        <Column style={{ minWidth: '25rem' }} field={'gdsDs3'} header={'Goods Additional Description'} />
                         <Column style={{ minWidth: '12rem' }} field={'pckMrk1First'} header={'Package Mark1'} />
                         <Column style={{ minWidth: '12rem' }} field={'pckMrk2First'} header={'Package Mark2'} />
                         <Column field={'customsProc'} header={'Customs Procedure'} />
                         <Column field={'itemGrossWeight'} header={'Item Gross Weight'} />
                         <Column field={'itemNetWeight'} header={'Item Net Weight'} />
-                        <Column filter filterField='brokerTin' field={'brokerTin'} header={'Broker Tin'} />
-                        <Column style={{ minWidth: '10rem' }} field={'decNam'} header={'Declarant Name'} />
+                        <Column  field={'brokerTin'} header={'Broker Tin'} />
+                        <Column style={{ minWidth: '20rem' }} field={'decNam'} header={'Declarant Name'} />
                         <Column filter filterField='companyTin' field={'companyTin'} header={'Company Tin'} />
-                        <Column field={'cmpNam'} header={'Company Name'} />
-                        <Column style={{ minWidth: '14rem' }} field={'finNam'} header={'Financial Name'} />
+                        <Column style={{ minWidth: '20rem' }}field={'cmpNam'} header={'Company Name'} />
+                        <Column style={{ minWidth: '20rem' }} field={'finNam'} header={'Financial Name'} />
                         <Column field={'fisCod'} header={'Financial Code'} />
-                        <Column field={'countryDest'} header={'Destination Country Name'} />
+                        <Column style={{ minWidth: '12rem' }}field={'countryDest'} header={'Destination Country Name'} />
                         <Column field={'countryDestCod'} header={'Destination Country Code'} />
-                        <Column filter filterField="countryExport" field={'countryExport'} header={'Country of Export Name'} />
+                        <Column  field={'countryExport'} header={'Country of Export Name'} />
                         <Column field={'countryOrg'} header={'Country of Origin'} />
                         <Column field={'lorryTotal'} header={'Lorry Total'} />
                         <Column field={'currencyCode'} header={'Currency Code'} />
@@ -113,14 +114,14 @@ function DPS_4551() {
                         <Column field={'status'} header={'Status'} />
                         <Column field={'locGoods'} header={'Location of Goods'} />
                         <Column field={'licCodItem'} header={'Licence reference number'} />
-                        <Column style={{ minWidth: '14rem' }} field={'txtFreItem'} header={'Free Text'} />
-                        <Column style={{ minWidth: '12rem' }} field={'pckMrk1Second'} header={'Package Mark1'} />
-                        <Column style={{ minWidth: '12rem' }} field={'pckMrk2Second'} header={'Package Mark2'} />
+                        <Column style={{ minWidth: '12rem' }} field={'txtFreItem'} header={'Free Text'} />
+                        <Column style={{ minWidth: '20rem' }} field={'pckMrk1Second'} header={'pckMrk1Second'} />
+                        <Column style={{ minWidth: '20rem' }} field={'pckMrk2Second'} header={'pckMrk2Second'} />
                         <Column field={'tarVmtFirst'} header={'Valuation Method'} />
                         <Column style={{ minWidth: '12rem' }} field={'tarAtt'} header={'Attached Documents Codes'} />
                         <Column style={{ minWidth: '10rem' }} field={'tarVdt'} header={'Value details'} />
-                        <Column filter filterField="tarVmtSecond" field={'tarVmtSecond'} header={'Valuation Method Second'} />
-                        <Column filter filterField="gCategoryOfGoods1" field={'gCategoryOfGoods1'} header={'Nature of transaction - code 1'} />
+                        <Column  field={'tarVmtSecond'} header={'Valuation Method Second'} />
+                        <Column  field={'gCategoryOfGoods1'} header={'Nature of transaction - code 1'} />
                         <Column field={'gCategoryOfGoods2'} header={'Nature of transaction - code 2'} />
                     </DataTable>
                 </Box>
