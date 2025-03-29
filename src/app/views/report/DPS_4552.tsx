5c86e7d26f665b4ac50cc2052093007adb7f60c1
AliKhalid <ali16kalid@gmail.com>
2025-03-29 09:46:24 +0430

    bugs removed

--- src/app/views/report/DPS_4552.tsx
+++ src/app/views/report/DPS_4552.tsx
@@ -31,7 +31,6 @@ function DPS_4552() {
         customsCode: data.CustomsCode
       });
       if (res.data.length === 0) {
-        setLoading(false)
         setReportData([]);
       } else {
         setReportData(res.data);
@@ -44,28 +43,28 @@ function DPS_4552() {
   const basedOnOptions = [{
     label: 'declarant',
     name: 'declarant'
-  },{
+  }, {
     label: 'company',
     name: 'company'
-},{
-  label: 'Sad_Financial',
-  name: 'Sad_Financial'
-},{
-  label: 'Examiner',
-  name: 'Examiner'
-},{
-  label: 'I_no',
-  name: 'I_no'
-},{
-  label: 'P_no',
-  name: 'P_no'
-},{
-  label: 'M_no',
-  name: 'M_no'
-},{
-  label: 'HsCode',
-  name: 'HsCode'
-}]
+  }, {
+    label: 'Sad_Financial',
+    name: 'Sad_Financial'
+  }, {
+    label: 'Examiner',
+    name: 'Examiner'
+  }, {
+    label: 'I_no',
+    name: 'I_no'
+  }, {
+    label: 'P_no',
+    name: 'P_no'
+  }, {
+    label: 'M_no',
+    name: 'M_no'
+  }, {
+    label: 'HsCode',
+    name: 'HsCode'
+  }]
 
   return (
     <SimpleCard title="DPS_4552">
@@ -99,56 +98,56 @@ function DPS_4552() {
           showGridlines
           emptyMessage={'No Data Available'}
         >
-              <Column field={'sadType'}header ={'sadType'} />
-	            <Column field={'transportCustomsName'}header ={'transportCustomsName'}/>
-	            <Column field={'customsName'}header ={'customsName'}/>
-	            <Column field={'customsCode'}header ={'customsCode'}/>
-	            <Column field={'typeoftransport'}header= {'typeoftransport'}/>
-	            <Column field={'regSer'}header= {'regSer'}/>
-	            <Column field={'regNo'}header= {'regNo'}/>
-	            <Column field={'regDate;'}header= {'regDate;'}/>
-	            <Column field={'asmtNo'}header ={'asmtNo'}/>
-	            <Column field={'astDate'}header ={'astDate'}/>
-	            <Column field={'rcptNo'}header= {'rcptNo'}/>
-	            <Column field={'rcptDate'}header ={'rcptDate'}/>
-	            <Column style={{minWidth:"15rem"}}field={'bankNam'}header= {'bankNam'}/>
-	            <Column style={{minWidth:"15rem"}}field={'brokerTin'}header= {'brokerTin'}/>
-	            <Column style={{minWidth:"25rem"}}field={'decNam'}header= {'decNam'}/>
-	            <Column style={{minWidth:"15rem"}}field={'companyTin'}header= {'companyTin'}/>
-	            <Column style={{minWidth:"30rem"}}field={'cmpNam'}header= {'cmpNam'}/>
-	            <Column style={{minWidth:"30rem"}}field={'finNam'}header={'finNam'}/>
-	            <Column field={'fisCod'}header= {'fisCod'}/>
-	            <Column field={'itemTotal'}header= {'itemTotal'}/>
-	            <Column field={'itemNo'}header= {'itemNo'}/>
-	            <Column field={'hsCode'}header= {'hsCode'}/>
-	            <Column style={{minWidth:"30rem"}}field={'dsc'}header={'dsc'}/>
-	            <Column style={{minWidth:"30rem"}}field={'desc1'}header= {'desc1'}/>
-	            <Column style={{minWidth:"30rem"}}field={'MRK1'}header= {'MRK1'}/>
-	            <Column style={{minWidth:"30rem"}}field={'MRK2'}header= {'MRK2'}/>
-	            <Column field={'cpc'}header= {'cpc'}/>
-	            <Column field={'natProc'}header= {'natProc'}/>
-	            <Column field={'countryDest'}header= {'countryDest'}/>
-	            <Column field={'countryDestCod'}header= {'countryDestCod'}/>
-	            <Column field={'countryexport'}header= {'countryexport'}/>
-	            <Column field={'countryOrg'}header ={'countryOrg'}/>
-	            <Column field={'lorryTotal'}header ={'lorryTotal'}/>
-	            <Column field={'declarationValueCurrecy'}header= {'declarationValueCurrecy'}/>
-	            <Column field={'declarationValueAfs'}header={'declarationValueAfs'}/>
-	            <Column field={'declarationTaxes'}header ={'declarationTaxes'}/>
-	            <Column field={'status'}header= {'status'}/>
-	            <Column field={'locGoods'}header= {'locGoods'}/>
-	            <Column field={'licCod'}header ={'licCod'}/>
-	            <Column field={'freeTxt'}header= {'freeTxt'}/>
-	            <Column field={'brdCty'}header ={'brdCty'}/>
-	            <Column field={'brdNam'}header ={'brdNam'}/>
-	            <Column field={'dpaCty'}header={'dpaCty'}/>
-	            <Column field={'dpaNam'}header= {'dpaNam'}/>
-	            <Column field={'gcategoryOfGoods1'}header= {'gcategoryOfGoods1'}/>
-	            <Column field={'itemGrossWeight'}header= {'itemGrossWeight'}/>
-	            <Column field={'itemNetWeight'}header= {'itemNetWeight'}/>
-	            <Column field={'gcategoryOfGoods2'}header ={'gcategoryOfGoods2'}/>
-	            <Column field={'txtRsv'}header= {'txtRsv'}/>
-	            <Column field={'containerFlag'}header= {'containerFlag'}/>
+          <Column field={'sadType'} header={'sadType'} />
+          <Column field={'transportCustomsName'} header={'transportCustomsName'} />
+          <Column field={'customsName'} header={'customsName'} />
+          <Column field={'customsCode'} header={'customsCode'} />
+          <Column field={'typeoftransport'} header={'typeoftransport'} />
+          <Column field={'regSer'} header={'regSer'} />
+          <Column field={'regNo'} header={'regNo'} />
+          <Column field={'regDate;'} header={'regDate;'} />
+          <Column field={'asmtNo'} header={'asmtNo'} />
+          <Column field={'astDate'} header={'astDate'} />
+          <Column field={'rcptNo'} header={'rcptNo'} />
+          <Column field={'rcptDate'} header={'rcptDate'} />
+          <Column style={{ minWidth: "15rem" }} field={'bankNam'} header={'bankNam'} />
+          <Column style={{ minWidth: "15rem" }} field={'brokerTin'} header={'brokerTin'} />
+          <Column style={{ minWidth: "25rem" }} field={'decNam'} header={'decNam'} />
+          <Column style={{ minWidth: "15rem" }} field={'companyTin'} header={'companyTin'} />
+          <Column style={{ minWidth: "30rem" }} field={'cmpNam'} header={'cmpNam'} />
+          <Column style={{ minWidth: "30rem" }} field={'finNam'} header={'finNam'} />
+          <Column field={'fisCod'} header={'fisCod'} />
+          <Column field={'itemTotal'} header={'itemTotal'} />
+          <Column field={'itemNo'} header={'itemNo'} />
+          <Column field={'hsCode'} header={'hsCode'} />
+          <Column style={{ minWidth: "30rem" }} field={'dsc'} header={'dsc'} />
+          <Column style={{ minWidth: "30rem" }} field={'desc1'} header={'desc1'} />
+          <Column style={{ minWidth: "30rem" }} field={'MRK1'} header={'MRK1'} />
+          <Column style={{ minWidth: "30rem" }} field={'MRK2'} header={'MRK2'} />
+          <Column field={'cpc'} header={'cpc'} />
+          <Column field={'natProc'} header={'natProc'} />
+          <Column field={'countryDest'} header={'countryDest'} />
+          <Column field={'countryDestCod'} header={'countryDestCod'} />
+          <Column field={'countryexport'} header={'countryexport'} />
+          <Column field={'countryOrg'} header={'countryOrg'} />
+          <Column field={'lorryTotal'} header={'lorryTotal'} />
+          <Column field={'declarationValueCurrecy'} header={'declarationValueCurrecy'} />
+          <Column field={'declarationValueAfs'} header={'declarationValueAfs'} />
+          <Column field={'declarationTaxes'} header={'declarationTaxes'} />
+          <Column field={'status'} header={'status'} />
+          <Column field={'locGoods'} header={'locGoods'} />
+          <Column field={'licCod'} header={'licCod'} />
+          <Column field={'freeTxt'} header={'freeTxt'} />
+          <Column field={'brdCty'} header={'brdCty'} />
+          <Column field={'brdNam'} header={'brdNam'} />
+          <Column field={'dpaCty'} header={'dpaCty'} />
+          <Column field={'dpaNam'} header={'dpaNam'} />
+          <Column field={'gcategoryOfGoods1'} header={'gcategoryOfGoods1'} />
+          <Column field={'itemGrossWeight'} header={'itemGrossWeight'} />
+          <Column field={'itemNetWeight'} header={'itemNetWeight'} />
+          <Column field={'gcategoryOfGoods2'} header={'gcategoryOfGoods2'} />
+          <Column field={'txtRsv'} header={'txtRsv'} />
+          <Column field={'containerFlag'} header={'containerFlag'} />
         </DataTable>
       </ Box>
       <Toast ref={toastRef} />
--- src/app/views/report/DPS_4579.tsx
+++ src/app/views/report/DPS_4579.tsx
@@ -21,7 +21,6 @@ function DPS_4579() {
         customsCode: data.CustomsCode
       });
       if (res.data.length === 0) {
-        setLoading(false)
         setReportData([]);
       } else {
         setReportData(res.data);
@@ -55,31 +54,31 @@ function DPS_4579() {
           showGridlines
           emptyMessage={'No Data Available'}
         >
-<Column field={'IDENTIFIED_DATE_TIME'} header={'IDENTIFIED_DATE_TIME'}/>
-<Column field={'EXPORTED_TO_XML_DATE_TIME'} header={'EXPORTED_TO_XML_DATE_TIME'}/>
-<Column field={'IMPORTED_FROM_XML_DATE_TIME'} header={'IMPORTED_FROM_XML_DATE_TIME'}/>
-<Column field={'DOCUMENT_ID'} header={'DOCUMENT_ID'}/>
-<Column field={'REF_NO'} header={'REF_NO'}/>
-<Column field={'TOTAL_NUMBER_OF_PACKAGES'} header={'TOTAL_NUMBER_OF_PACKAGES'}/>
-<Column field={'COUNTRY_OF_DESTINATION'} header={'COUNTRY_OF_DESTINATION'}/>
-<Column field={'COUNTRY_OF_ORIGIN'} header={'COUNTRY_OF_ORIGIN'}/>
-<Column field={'DATE_OF_DECLARATION'} header={'DATE_OF_DECLARATION'}/>
-<Column field={'TYPE_OF_DECLARATION'} header={'TYPE_OF_DECLARATION'}/>
-<Column field={'CONTAINER_FLAG'} header={'CONTAINER_FLAG'}/>
-<Column field={'TRANSPORT_MODE_AT_BORDER'} header={'TRANSPORT_MODE_AT_BORDER'}/>
-<Column field={'TIN_DECLARANT'} header={'TIN_DECLARANT'}/>
-<Column field={'TIN_CONSIGNEE'} header={'TIN_CONSIGNEE'}/>
-<Column field={'TIN_FIN'} header={'TIN_FIN'}/>
-<Column field={'IDENTITY_ATDEPARTURE'} header={'IDENTITY_ATDEPARTURE'}/>
-<Column field={'IDENTITY_CROSSINGBORDER'} header={'IDENTITY_CROSSINGBORDER'}/>
-<Column field={'cuo_officeofexport'} header={'cuo_officeofexport'}/>
-<Column field={'cuo_officeofexit'} header={'cuo_officeofexit'}/>
-<Column field={'FINANCIAL_VALUE'} header={'FINANCIAL_VALUE'}/>
-<Column field={'CURRENCY'} header={'CURRENCY'}/>
-<Column field={'ITEM_NUMBER'} header={'ITEM_NUMBER'}/>
-<Column field={'COMBINED_NOMENCLATURE'} header={'COMBINED_NOMENCLATURE'}/>
-<Column field={'GOODS_DESCRIPTION'} header={'GOODS_DESCRIPTION'}/>
-<Column field={'GROSS_MASS'} header={'GROSS_MASS'}/>
+          <Column field={'IDENTIFIED_DATE_TIME'} header={'IDENTIFIED_DATE_TIME'} />
+          <Column field={'EXPORTED_TO_XML_DATE_TIME'} header={'EXPORTED_TO_XML_DATE_TIME'} />
+          <Column field={'IMPORTED_FROM_XML_DATE_TIME'} header={'IMPORTED_FROM_XML_DATE_TIME'} />
+          <Column field={'DOCUMENT_ID'} header={'DOCUMENT_ID'} />
+          <Column field={'REF_NO'} header={'REF_NO'} />
+          <Column field={'TOTAL_NUMBER_OF_PACKAGES'} header={'TOTAL_NUMBER_OF_PACKAGES'} />
+          <Column field={'COUNTRY_OF_DESTINATION'} header={'COUNTRY_OF_DESTINATION'} />
+          <Column field={'COUNTRY_OF_ORIGIN'} header={'COUNTRY_OF_ORIGIN'} />
+          <Column field={'DATE_OF_DECLARATION'} header={'DATE_OF_DECLARATION'} />
+          <Column field={'TYPE_OF_DECLARATION'} header={'TYPE_OF_DECLARATION'} />
+          <Column field={'CONTAINER_FLAG'} header={'CONTAINER_FLAG'} />
+          <Column field={'TRANSPORT_MODE_AT_BORDER'} header={'TRANSPORT_MODE_AT_BORDER'} />
+          <Column field={'TIN_DECLARANT'} header={'TIN_DECLARANT'} />
+          <Column field={'TIN_CONSIGNEE'} header={'TIN_CONSIGNEE'} />
+          <Column field={'TIN_FIN'} header={'TIN_FIN'} />
+          <Column field={'IDENTITY_ATDEPARTURE'} header={'IDENTITY_ATDEPARTURE'} />
+          <Column field={'IDENTITY_CROSSINGBORDER'} header={'IDENTITY_CROSSINGBORDER'} />
+          <Column field={'cuo_officeofexport'} header={'cuo_officeofexport'} />
+          <Column field={'cuo_officeofexit'} header={'cuo_officeofexit'} />
+          <Column field={'FINANCIAL_VALUE'} header={'FINANCIAL_VALUE'} />
+          <Column field={'CURRENCY'} header={'CURRENCY'} />
+          <Column field={'ITEM_NUMBER'} header={'ITEM_NUMBER'} />
+          <Column field={'COMBINED_NOMENCLATURE'} header={'COMBINED_NOMENCLATURE'} />
+          <Column field={'GOODS_DESCRIPTION'} header={'GOODS_DESCRIPTION'} />
+          <Column field={'GROSS_MASS'} header={'GROSS_MASS'} />
         </DataTable>
       </ Box>
     </SimpleCard>
